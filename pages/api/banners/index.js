1968dd189b6166ac18596b6c44d8536c77de3684
dimas.syarief <defitramuhamadyasin20@gmail.com>
2023-01-18 00:54:28 +0700

    axios downgrade

--- pages/api/banners/index.js
+++ pages/api/banners/index.js
@@ -25,13 +25,52 @@ export default async function handler(req, res) {
 //             , data: request
 //         };
 // await axios.request(axiosConfig);
-            const { data } = await axios.post(process.env.REACT_APP_DITOKOKU_API_BASE_URL + process.env.REACT_APP_DITOKOKU_API_VERSION_URL + "/banners/upload-image", req, {
-                responseType: "stream",
-                headers: {
-                "Content-Type": req.headers["content-type"]
-                },
-            });
-            data.pipe(res);
+
+            // const { data } = await axios.post(process.env.REACT_APP_DITOKOKU_API_BASE_URL + process.env.REACT_APP_DITOKOKU_API_VERSION_URL + "/banners/upload-image", req, {
+            //     responseType: "stream",
+            //     headers: {
+            //     "Content-Type": req.headers["content-type"]
+            //     },
+            // });
+            
+            let dataResults;
+            try{
+                //2 . Execute AXIOS untuk menambahkan informasi Product
+                dataResults = await axios.post(process.env.REACT_APP_DITOKOKU_API_BASE_URL + process.env.REACT_APP_DITOKOKU_API_VERSION_URL + "/banners/upload-image", req, {
+                    responseType: "stream",
+                    headers: {
+                    "Content-Type": req.headers["content-type"]
+                    },
+                });
+            }catch(error){
+                // console.log("error:");console.log(error);
+                //3 . Error Handler untuk kegagalan koneksi ke MicroService Enterprise Structure
+                let errorContent= {};
+                if(error.code === "ECONNREFUSED") {
+                    //3.1 Error Handler untuk kegagalan koneksi ke MicroService Enterprise Structure
+                    errorContent ={
+                        status_code: 500,
+                        timestamp: '',
+                        error_title: "Internal Server Error",
+                        error_message: "The product service on this system is down for maintenance. Please contact your system administrator.",
+                    }
+                }else{
+                    //3.2 Error Handler untuk pesan gagal dari internal MicroService Enterprise Structure
+                    if (error.response != null){
+                        //Error Message dari Microservice Enterprise Structure
+                        errorContent = error.response.data;
+                        errorContent.status_code = error.response.status;
+                    }
+                }
+                throw errorContent;
+            }
+    
+            //4 . Send Response berupa informasi Enterprise Structure
+            const result = {
+                "status_code": 200
+            }
+    
+            res.status(result.status_code).send(result);
             break
         case 'PATCH':
             // Edit
