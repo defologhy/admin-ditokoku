434bcea54b32dba0fe1b25124c0b52edf8b19b0e
dimas.syarief <defitramuhamadyasin20@gmail.com>
2023-01-17 09:34:16 +0700

    cors rollback

--- next.config.js
+++ next.config.js
@@ -32,21 +32,7 @@ const nextConfig = {
     REACT_APP_COOKIE_EXPIRES: process.env.REACT_APP_COOKIE_EXPIRES
   },
   reactStrictMode: true,
-  swcMinify: true,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/banners/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+  swcMinify: true
 }
 
 
